26e5f6db79bbe7b4e6957956ace3fc3202771447
Douglas Ryan Terleski <103542806+DougDevs@users.noreply.github.com>
2023-08-07 21:46:01 -0300

    Update services.ts

--- src/services/services.ts
+++ src/services/services.ts
@@ -38,14 +38,14 @@ export const apiServices = [
 {
     id: 1,
     name:"Somos uma Empresa Profissional em Desenvolver Sites",
-    description: "Oferecemos serviços especializados em desenvolvimento de sites de alta qualidade e desempenho para empresas de todos os segmentos..",
+    description: "Oferecemos serviços especializados em desenvolvimento de sites de alta qualidade e desempenho para empresas de todos os segmentos.",
   }
 ];
 export const apiPricing = [
   {
     id: 1,
     name: "Web Site",
-    description: " O Web Site Classe C é a opção perfeita para empresas e negócios que desejam uma presença online básica, mas efetiva. Com informações essenciais sobre sua empresa e imagens relevantes, esse site oferece aos clientes uma forma simples e acessível de conhecer seus produtos e serviços. Por apenas $500, você terá um site funcional e capaz de atender às necessidades básicas do seu negócio na web.",
+    description: " O Web Site Classe C é a opção perfeita para empresas e negócios que desejam uma presença online básica, mas efetiva. Com informações essenciais sobre sua empresa e imagens relevantes, esse site oferece aos clientes uma forma simples e acessível de conhecer seus produtos e serviços.",
     item1: "Profissional",
     item2: "Credibilidade",
     item3: "Marketing",
@@ -53,7 +53,7 @@ export const apiPricing = [
   {
     id: 2,
     name: "Site Startup",
-    description: "O Site Startup Classe B é a escolha ideal para empreendedores e empresas em crescimento. Além de conter todas as informações relevantes sobre sua empresa, ele apresenta detalhes sobre seus produtos e serviços, acompanhados de fotos profissionais que transmitem credibilidade e profissionalismo. Por apenas $750, você terá um site moderno e atrativo que ajudará a impulsionar seu negócio de forma elegante e diferenciada.",
+    description: "O Site Startup Classe B é a escolha ideal para empreendedores e empresas em crescimento. Além de conter todas as informações relevantes sobre sua empresa, ele apresenta detalhes sobre seus produtos e serviços, acompanhados de fotos profissionais que transmitem credibilidade e profissionalismo.",
     item1: "Moderno",
     item2: "Digital",
     item3: "Inovador",
@@ -61,7 +61,7 @@ export const apiPricing = [
   {
     id: 3,
     name: "Portal na web",
-    description: "O Portal na Web Classe A é a solução completa para empresas de alto nível que desejam se destacar no mundo digital. Com informações detalhadas sobre a empresa, produtos e serviços, acompanhadas de imagens cuidadosamente capturadas por profissionais, esse site oferece uma experiência de navegação completa e envolvente. Por apenas $1000, você terá um site sofisticado e impactante que impulsionará sua marca para o próximo nível.",
+    description: "O Portal na Web Classe A é a solução completa para empresas de alto nível que desejam se destacar no mundo digital. Com informações detalhadas sobre a empresa, produtos e serviços, acompanhadas de imagens cuidadosamente capturadas por profissionais, esse site oferece uma experiência de navegação completa e envolvente.",
     item1: "Amplo",
     item2: "Performance",
     item3: "Finesse",
